6cdea0a73688db4204db521de95d85a8f3e5deb1
yinyihao1 <369987197@qq.com>
2017-07-31 22:47:38 +0800

    单链表类

--- 单链表/Program.cs
+++ 单链表/Program.cs
@@ -10,6 +10,48 @@ namespace 单链表
     {
         static void Main(string[] args)
         {
+            int _data = 0;
+            Node<int> headNode = new Node<int>() { Data = _data++ };
+            for (int i = 0; i < 10; i++)
+            {
+                headNode.Add(headNode, _data++);
+            }
+            Console.WriteLine(headNode.GetLastNode(headNode).Data);
+        }
+    }
+
+    /// <summary>
+    /// 单链表节点
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+
+        public void Add(Node<T> headNode,T data)
+        {
+            Node<T> add_node = new Node<T>() { Data=data};
+            headNode.GetLastNode(headNode).Next = add_node;
+        }
+
+        public Node<T> GetLastNode(Node<T> headNode)
+        {
+            if (headNode.Next == null)
+            {
+                return headNode;
+            }
+            //else
+            //{
+            //    return GetLastNode(headNode.Next);
+            //}
+            //or 
+            while (headNode.Next!=null)
+            {
+                headNode= GetLastNode(headNode.Next);
+            }
+            return headNode;
         }
     }
 }
